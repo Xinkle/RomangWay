47d5f7b79e1b6f3eefbfe1b343a169a90e791c36
Xinkle <cswcsy@gmail.com>
2022-10-15 19:03:51 +0900

    Implement ordering in CommandTeaching DAO

--- src/main/kotlin/database/CommandTeachingDao.kt
+++ src/main/kotlin/database/CommandTeachingDao.kt
@@ -1,11 +1,14 @@
 package database
 
+import org.jetbrains.exposed.sql.SortOrder
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.like
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun findCommand(commandName: String): CommandTeaching? = transaction {
-    CommandTeaching.find(CommandTeachingTable.name eq commandName).firstOrNull()
+    CommandTeaching.find(CommandTeachingTable.name eq commandName)
+        .orderBy(CommandTeachingTable.createDate to SortOrder.DESC)
+        .firstOrNull()
 }
 
 fun findSimilarCommands(commandName: String): List<CommandTeaching> = transaction {
