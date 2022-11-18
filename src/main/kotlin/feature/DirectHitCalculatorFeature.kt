3a1f9b11bda1120d3eff560cd98457687a4e0dac
Xinkle <cswcsy@gmail.com>
2022-11-19 01:12:44 +0900

    Version up to 1.07

--- src/main/kotlin/feature/DirectHitCalculatorFeature.kt
+++ src/main/kotlin/feature/DirectHitCalculatorFeature.kt
@@ -1,9 +1,15 @@
 package feature
 
+import dev.kord.common.entity.ButtonStyle
+import dev.kord.common.entity.TextInputStyle
 import dev.kord.core.Kord
 import dev.kord.core.behavior.interaction.response.respond
 import dev.kord.core.entity.interaction.GuildChatInputCommandInteraction
+import dev.kord.rest.builder.component.ActionRowBuilder
+import dev.kord.rest.builder.component.ButtonBuilder
+import dev.kord.rest.builder.component.TextInputBuilder
 import dev.kord.rest.builder.interaction.int
+import dev.kord.rest.builder.message.modify.InteractionResponseModifyBuilder
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
@@ -39,9 +45,41 @@ class DirectHitCalculatorFeature(private val kord: Kord) : CoroutineScope, Guild
         val calculatedDirectHit = ((550 * (directHit - 400) / 1900.0)).toInt() / 10.0
         val nextDirectHit = ceil((calculatedDirectHit + 0.1) * 1900 / 55) + 400
 
-        response.respond {
-            content =
-                "현재 직격확률은 $calculatedDirectHit%, 데미지 기대값은 ${calculatedDirectHit * 0.0025 + 1}배, 다음단계를 위한 직격수치는 $nextDirectHit 입니다!"
+        try {
+            val builder = InteractionResponseModifyBuilder().apply {
+                content =
+                    "현재 직격확률은 $calculatedDirectHit%, 데미지 기대값은 ${calculatedDirectHit * 0.0025 + 1}배, 다음단계를 위한 직격수치는 $nextDirectHit 입니다!"
+                components =
+                    mutableListOf(
+                        ActionRowBuilder().apply {
+                            textInput(
+                                TextInputStyle.Short,
+                                "test",
+                                "FightID"
+                            )
+                        }
+                    )
+            }
+
+            println(builder.toRequest().toString())
+
+            response.respond {
+                content =
+                    "현재 직격확률은 $calculatedDirectHit%, 데미지 기대값은 ${calculatedDirectHit * 0.0025 + 1}배, 다음단계를 위한 직격수치는 $nextDirectHit 입니다!"
+                components =
+                    mutableListOf(
+                        ActionRowBuilder().apply {
+                            this.
+                            textInput(
+                                TextInputStyle.Short,
+                                "test",
+                                "FightID"
+                            )
+                        }
+                    )
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 }
